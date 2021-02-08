8b23eef1e1f3d8aeffaddd5456cca628c9f18530
Cameron Thompson <camthomp96@gmail.com>
2021-02-08 01:42:09 -0700

    Routes for pages

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,17 @@
 
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Welcome Page
-router.get('/', (req, res) => res.render('index'));
+router.get('/', (req, res) => res.render('index', { user: req.user }));
+router.get('/projects', (req, res) => res.render('projects', { user: req.user }));
+router.get('/addProject', ensureAuthenticated, (req, res) => res.render('addProject', {user: req.user }));
 
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', { user: req.user, auth: true });
+});
 
 
 module.exports = router;
